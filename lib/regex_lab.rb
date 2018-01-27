ffb95fa726553e53a8d3c247007becc53b37fd80
Randall Harris Haynes <harrishaynes@gmail.com>
2018-01-27 02:48:53 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.scan(/[\d]/).all?{|n| n.length==10}
+  phone.join(" ").scan(/[\d]/).all?{|n| n.length==10}
 end
 
 valid_phone_number?(["2438894546", "(718)891-1313", "234 435 9978", "(800)4261134"])

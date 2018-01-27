92869ae0dc7fec0167c893bbefa356bc01da361c
Randall Harris Haynes <harrishaynes@gmail.com>
2018-01-27 02:20:22 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,9 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
+  !(text.scan().empty?)
 end
 
 def valid_phone_number?(phone)
+  !(phone.scan().empty?)
 end

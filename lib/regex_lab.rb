4f83ea2a8fca0efdcef97d6823d311fa32c388c8
Randall Harris Haynes <harrishaynes@gmail.com>
2018-01-27 03:09:12 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-
+  !(word.scan(/^[aeiouAEIOU]/).empty?)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun+\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  !(text.scan(/^[A-Z]+[\w\W]+[.?!]\z/).empty?)
 end
 
 def valid_phone_number?(phone)
-
+  phone.scan(/[\d]/).length==10
 end

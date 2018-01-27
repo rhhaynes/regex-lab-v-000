dcd76134015e19794a12f5722c579e7b8bf4fe44
Randall Harris Haynes <harrishaynes@gmail.com>
2018-01-27 02:45:34 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.scan(/[\d]/).all?{|n| n.length==9}
+  phone.scan(/[\d]/).all?{|n| n.length==10}
 end
